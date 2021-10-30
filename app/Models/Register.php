90e0b70b087d724f9bcc9046d00eda79e2bdc744
Eduardo Berg <77705253+Piphiz@users.noreply.github.com>
2021-10-30 13:25:22 -0300

    Configurando Models

---
+++ app/Models/Register.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Register extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'userd_id',
+        'vaccine_id',
+        'dose_number',
+        'next_dose',
+    ];
+
+    protected $hidden = [];
+
+    protected $casts = [];
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,41 +4,25 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'cpf',
+        'phone',
+        'address',
+        'age'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = [];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = [];
 }
